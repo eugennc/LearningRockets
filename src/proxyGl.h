5a3357059eea8c8188077e1f362b2ac9c54a9a79
Eugen Croitoru <eugennc@gmail.com>
2019-05-17 18:02:30 +0300

    Added comment to help with symbol overdefinition debugging.

--- src/proxyGl.h
+++ src/proxyGl.h
@@ -3,6 +3,7 @@
 
 //this should be the only place the followind declarations take place; this is mainly to allow modifiying a single place, conflicts and errors may or may not occur
 //glew is the first GL-related header to include
+//try commenting out glu.h and glext.h if you get symbol redefinition errors
 #define GLEW_STATIC
 #include <GL/glew.h>
 //probably redundant, thanks to glew.h
